41db7df101cb794bd6d2c199fab02fe1faa33d0b
jiancaballero <jiancaballero@gmail.com>
2022-11-20 02:13:40 +0900

    env file added

--- scripts/app.js
+++ scripts/app.js
@@ -2,19 +2,27 @@ const email = document.getElementById("email");
 const subject = document.getElementById("subject");
 const message = document.getElementById("message");
 const submitEmailBtn = document.getElementById("contact-submit-button");
+require("dotenv").config();
+submitEmailBtn.addEventListener("click", send);
 
-submitEmailBtn.addEventListener("click", sendEmail);
-function sendEmail() {
-    Email.send({
-      Host: "smtp.gmail.com",
-      Username: email.value,
-      Password: "Enter your password",
-      To: 'jiancaballero@gmail.com',
-      From: email.value,
-      Subject: subject.value,
-      Body: message.value,
-    })
-      .then(function (message) {
-        alert("mail sent successfully")
-      });
+// IMPORT NODEMAILER
+const nodemailer = require("nodemailer");
+const transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+      user: process.env.EMAIL,
+      pass: process.env.PASSWORD
   }
+});
+
+
+async function send() {
+  const result = await transporter.sendMail({
+      from: email.value,
+      to: process.env.EMAIL,
+      subject: subject.value,
+      text: message.value
+  });
+
+  console.log(JSON.stringify(result, null, 4));
+}
